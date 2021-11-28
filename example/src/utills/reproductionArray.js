04567ab1eea92078bdca12201607aacefb31d861
Ivan Admaers <admaers@icloud.com>
2021-11-28 14:18:28 +0400

    add a function to reproduction array

---
+++ example/src/utills/reproductionArray.js
@@ -0,0 +1,7 @@
+const reproductionArray = (array = [], length = 0) => [
+  ...Array(length)
+    .fill('_')
+    .map(() => array[Math.floor(Math.random() * array.length)]),
+];
+
+export default reproductionArray;
