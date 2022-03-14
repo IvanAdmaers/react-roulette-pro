fdbb58a699bcd2e73208389ba3073099c349f29d
Ivan Admaers <admaers@icloud.com>
2022-03-14 16:34:43 +0400

    add a test

--- src/hooks/useAudio/useAudio.test.js
+++ src/hooks/useAudio/useAudio.test.js
@@ -45,4 +45,45 @@ describe('useAudio hook', () => {
 
     expect(screen.getByText(/true/i));
   });
+
+  it('should start and stop playing sound', () => {
+    const Component = () => {
+      const { isPlaying, start, stop } = useAudio(sound);
+
+      const handlePlay = () => {
+        start();
+      };
+
+      const handleStop = () => {
+        stop();
+      };
+
+      return (
+        <>
+          <button onClick={handlePlay} type="button">
+            Play
+          </button>
+          <button onClick={handleStop} type="button">
+            Stop
+          </button>
+          <p>Is palying: {`${isPlaying}`}</p>
+        </>
+      );
+    };
+
+    render(<Component />);
+
+    window.HTMLMediaElement.prototype.play = () => null;
+    window.HTMLMediaElement.prototype.pause = () => null;
+
+    const [startButton, stopButton] = screen.getAllByRole('button');
+
+    userEvent.click(startButton);
+
+    expect(screen.getByText(/true/i));
+
+    userEvent.click(stopButton);
+
+    expect(screen.getByText(/false/i));
+  });
 });
