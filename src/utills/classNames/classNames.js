6bb21c73482c9e4eba299b747b16530afa0e36fe
Ivan Admaers <admaers@icloud.com>
2022-07-16 12:31:18 +0400

    improvements

--- src/utills/classNames/classNames.js
+++ src/utills/classNames/classNames.js
@@ -1,28 +1,109 @@
+const isArray = (item) => Array.isArray(item);
+
+const isObject = (item) =>
+  item !== null && isArray(item) === false && typeof item === 'object';
+
+const isFunction = (item) => typeof item === 'function';
+
+const isBoolean = (item) => typeof item === 'boolean';
+
+const hasOwnPrototypeOfToString = (object) =>
+  object.toString !== Object.prototype.toString;
+
 const classNames = (...classes) => {
-  let className = '';
+  let resultClassName = '';
+
+  const handlePrimitive = (item, asKey) => {
+    const isItemBoolean = isBoolean(item);
+
+    if (!item || isItemBoolean === true) {
+      return;
+    }
+
+    resultClassName += `${asKey !== undefined ? asKey : item} `;
+  };
+
+  const handleObject = (object) => {
+    const entries = Object.entries(object);
+
+    /* maybe throw it out */
+    const hasObjectOwnPrototypeOfToString = hasOwnPrototypeOfToString(object);
+    /**/
+
+    entries.forEach(([key, value]) => {
+      if (!value) {
+        return;
+      }
+
+      const isValueFunction = isFunction(value);
+      const isToString = key === 'toString';
+
+      if (isValueFunction === true && isToString === false) {
+        const functionResult = value();
+
+        handlePrimitive(functionResult, key);
+
+        return;
+      }
+
+      if (isToString === false) {
+        resultClassName += `${key} `;
+      }
+    });
+
+    /* maybe throw it out */
+    if (hasObjectOwnPrototypeOfToString === true) {
+      const toStringValue = object.toString();
+
+      handlePrimitive(toStringValue);
+    }
+    /**/
+  };
+
+  const handleArray = (array) => {
+    array.forEach((item) => {
+      if (!item || typeof item === 'boolean') {
+        return;
+      }
+
+      const isItemArray = isArray(item);
+      const isItemObject = isObject(item);
+
+      if (isItemArray === true) {
+        return handleArray(item);
+      }
+
+      if (isItemObject === true) {
+        return handleObject(item);
+      }
+
+      resultClassName += `${item} `;
+    });
+  };
 
   for (let i = 0; i < classes.length; i += 1) {
     const classNameItem = classes[i];
 
-    if (classNameItem !== null && typeof classNameItem === 'object') {
-      const [key] = Object.keys(classNameItem);
-      const [value] = Object.values(classNameItem);
+    const isItemObject = isObject(classNameItem);
 
-      if (value) {
-        className += `${key} `;
+    const isItemArray = isArray(classNameItem);
 
-        continue;
-      }
+    if (isItemObject === true) {
+      handleObject(classNameItem);
 
       continue;
     }
 
-    if (classNameItem) {
-      className += `${classNameItem} `;
+    if (isItemArray === true) {
+      handleArray(classNameItem);
+
+      continue;
     }
+
+    handlePrimitive(classNameItem);
   }
 
-  return className;
+  return resultClassName.trim();
 };
 
 export default classNames;
