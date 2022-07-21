1ad4d6cd5313515e8bad3d9a47724483363cb7bb
Ivan Admaers <admaers@icloud.com>
2022-07-21 11:30:34 +0400

    rename

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import RoulettePro from './components/Roulette';
+import type { IDesignPluginProps, IDesignPlugin } from './types';
+
+export default RoulettePro;
+
+export { IDesignPluginProps, IDesignPlugin };
