79124abcbcc2e270ed96619f739723fcef81499a
Ivan Admaers <admaers@icloud.com>
2022-08-28 19:28:14 -0400

    add export of PrizeType

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import RoulettePro from './components/Roulette';
-import type { IDesignPluginProps, IDesignPlugin } from './types';
+import type { IDesignPluginProps, IDesignPlugin, PrizeType } from './types';
 
 export default RoulettePro;
 
-export { IDesignPluginProps, IDesignPlugin };
+export { IDesignPluginProps, IDesignPlugin, PrizeType };
